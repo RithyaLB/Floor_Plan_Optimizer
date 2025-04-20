93c55093eb51ce7243aad7cbe71f1ffba7c11e5a
RithyaLB <rithyabashar25@gmail.com>
2025-04-20 12:06:06 +0530

    Firstly commit

---
+++ backend/app.py
@@ -0,0 +1,26 @@
+from flask import Flask, request, jsonify, render_template
+import os
+
+app = Flask(__name__)
+
+UPLOAD_FOLDER = 'static/uploads'
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        file = request.files['file']
+        if file:
+            filepath = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+            file.save(filepath)
+            return jsonify({"message": "File uploaded", "path": filepath})
+    return '''
+        <form method="post" enctype="multipart/form-data">
+            <input type="file" name="file">
+            <input type="submit">
+        </form>
+    '''
+
+if __name__ == '__main__':
+    app.run(debug=True)
